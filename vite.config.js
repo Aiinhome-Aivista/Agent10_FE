0809f4e7d93c5d4c603c52eb12ce66b95094f7c3
Purnendu Dey <rdey07344@gmail.com>
2026-09-10 13:17:34 +0530

    link replace .env

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-    '/api': {
-    target: 'http://127.0.0.1:8002',
-    changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const targetUrl = env.VITE_API_BASE_URL
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: targetUrl,
+          changeOrigin: true
+        }
       }
     }
   }
